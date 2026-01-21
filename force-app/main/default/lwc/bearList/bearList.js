ba22f655aee98e35602542681899a3ba44212792
Mee <me@example.com>
2026-01-20 21:46:51 -0600

    modified:   force-app/main/default/lwc/bearList/bearList.js

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,10 +1,23 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement } from 'lwc';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
 /** BearController.getAllBears() Apex method */
 import getAllBears from '@salesforce/apex/BearController.getAllBears';
 export default class BearList extends LightningElement {
-	@wire(getAllBears) bears;
+	bears;
+	error;
 	appResources = {
 		bearSilhouette: `${ursusResources}/standing-bear-silhouette.png`,
 	};
+	connectedCallback() {
+		this.loadBears();
+	}
+	loadBears() {
+		getAllBears()
+			.then(result => {
+				this.bears = result;
+			})
+			.catch(error => {
+				this.error = error;
+			});
+	}
 }
